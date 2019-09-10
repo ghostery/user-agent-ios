61fd9dd6de5631cd58490fa9a185a65fa0664b30
Krzysztof Jan Modras <chrmod@chrmod.net>
2019-09-10 15:03:49 +0200

    Removing unused resources
    * Mozilla fonts (use System fonts instead)
    * Top Sites logos
    * other small resources

--- ReactNative/ViewControllers/HomeViewController.swift
+++ ReactNative/ViewControllers/HomeViewController.swift
@@ -16,16 +16,12 @@ class HomeViewController: ReactViewController {
     init(profile: Profile) {
         self.profile = profile
         super.init(componentName: "Home")
+        // TODO: user proper caching
+        self.profile.panelDataObservers.activityStream.refreshIfNeeded(forceTopSites: true)
     }
 
     required init?(coder aDecoder: NSCoder) {
         fatalError("init(coder:) has not been implemented")
-    }
-
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        // TODO: user proper caching
-        self.profile.panelDataObservers.activityStream.refreshIfNeeded(forceTopSites: true)
     }
 }
 
