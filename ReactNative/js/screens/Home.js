f7134561bc57c2839717ddce5302892a2cdcb2f6
Daniel Jilg <winsmith@winsmith.de>
2019-09-12 15:06:31 +0200

    Improve styling of home view

--- ReactNative/js/screens/Home.js
+++ ReactNative/js/screens/Home.js
@@ -1,6 +1,9 @@
 import React, { useState, useEffect } from 'react';
 import {
   NativeModules,
+  View,
+  Text,
+  StyleSheet,
 } from 'react-native';
 import { FlatGrid } from 'react-native-super-grid';
 import SpeedDial from '../components/SpeedDial';
@@ -21,18 +24,34 @@ export const useSpeedDials = () => {
 
 const openSpeedDialLink = speedDial => NativeModules.BrowserActions.openLink(speedDial.url, "", false);
 
+const styles = StyleSheet.create({
+  container: {
+    marginTop: 8,
+  },
+  header: {
+    fontSize: 28,
+    marginLeft: 12,
+    marginRight: 12,
+  },
+});
+
 export default function Home() {
   const speedDials = useSpeedDials();
   return (
-    <FlatGrid
-      itemDimension={80}
-      items={speedDials}
-      renderItem={({ item: speedDial }) =>
-        SpeedDial({
-          speedDial,
-          onPress: openSpeedDialLink,
-        })
-      }
-    />
+    <View style={styles.container}>
+      <Text style={styles.header}>
+        Most visited
+      </Text>
+      <FlatGrid
+        itemDimension={80}
+        items={speedDials}
+        renderItem={({ item: speedDial }) =>
+          SpeedDial({
+            speedDial,
+            onPress: openSpeedDialLink,
+          })
+        }
+      />
+    </View>
   )
 }
