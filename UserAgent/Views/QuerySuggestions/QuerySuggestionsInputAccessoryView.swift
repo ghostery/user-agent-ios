d12a75a254c43716d4ef0e5817e795c948c807df
Krzysztof Jan Modras <chrmod@chrmod.net>
2019-10-02 16:39:29 +0200

    Search: show query suggestions - fixes #190

---
+++ UserAgent/Views/QuerySuggestions/QuerySuggestionsInputAccessoryView.swift
@@ -0,0 +1,78 @@
+//
+// Copyright (c) 2017-2019 Cliqz GmbH. All rights reserved.
+//
+// This Source Code Form is subject to the terms of the Mozilla Public
+// License, v. 2.0. If a copy of the MPL was not distributed with this
+// file, You can obtain one at https://mozilla.org/MPL/2.0/.
+//
+
+import UIKit
+
+protocol QuerySuggestionDelegate: class {
+    func querySuggestionTapped(_ suggestion: String)
+}
+
+class QuerySuggestionsInputAccessoryView: UIView {
+    static let ShowSuggestionsNotification = NSNotification.Name(rawValue: "ShowSuggestionsNotification")
+
+    public weak var delegate: QuerySuggestionDelegate?
+
+    private var suggestionsView: SuggestionsView!
+
+    // MARK:- Initialization
+    init() {
+        let screenBounds = UIScreen.main.bounds
+        let width = min(screenBounds.width, screenBounds.height)
+        let frame = CGRect(x: 0.0, y: 0.0, width: width, height: 44)
+
+        super.init(frame: frame)
+
+        self.isHidden = true
+        self.autoresizingMask = .flexibleWidth
+        self.backgroundColor = UIColor(rgb: 0xADB5BD)
+
+        addSuggestionsView(frame: frame)
+
+        NotificationCenter.default.addObserver(
+            self,
+            selector: #selector(showSuggestions),
+            name: type(of: self).ShowSuggestionsNotification,
+            object: nil)
+    }
+
+    required init?(coder aDecoder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+
+    deinit {
+        NotificationCenter.default.removeObserver(self)
+    }
+
+    // MARK:- Private Helpers
+    @objc private func showSuggestions(notification: NSNotification) {
+        guard
+            let suggestionsData = notification.object as? [String: AnyObject],
+            let query = suggestionsData["query"] as? String,
+            let suggestions = suggestionsData["suggestions"] as? [String]
+        else { return }
+
+        DispatchQueue.main.async {
+            if self.suggestionsView.displaySuggestions(query: query, suggestions: suggestions) {
+                self.isHidden = false
+            } else {
+                self.isHidden = true
+            }
+        }
+    }
+
+    private func addSuggestionsView(frame: CGRect) {
+        let suggestionsView = SuggestionsView()
+        suggestionsView.frame = frame
+        suggestionsView.autoresizingMask = .flexibleWidth
+        suggestionsView.onSuggestionTapped = { suggestion in
+            self.delegate?.querySuggestionTapped(suggestion)
+        }
+        addSubview(suggestionsView)
+        self.suggestionsView = suggestionsView
+    }
+}
---
+++ UserAgent/Views/QuerySuggestions/SuggestionsView.swift
@@ -0,0 +1,176 @@
+//
+// Copyright (c) 2017-2019 Cliqz GmbH. All rights reserved.
+//
+// This Source Code Form is subject to the terms of the Mozilla Public
+// License, v. 2.0. If a copy of the MPL was not distributed with this
+// file, You can obtain one at https://mozilla.org/MPL/2.0/.
+//
+
+import UIKit
+
+private extension String {
+    func trim() -> String {
+        let newString = self.trimmingCharacters(in: CharacterSet.whitespacesAndNewlines)
+        return newString
+    }
+}
+
+class SuggestionsView: UIView {
+    // MARK:- Instance variables
+    var onSuggestionTapped: ((_ suggestion: String) -> Void)?
+
+    // MARK:- Constants
+    private let boldFontAttributes = [
+        NSAttributedString.Key.font: UIFont.boldSystemFont(ofSize: 17),
+        NSAttributedString.Key.foregroundColor: UIColor.white,
+    ]
+    private let normalFontAttributes = [
+        NSAttributedString.Key.font: UIFont.systemFont(ofSize: 16),
+        NSAttributedString.Key.foregroundColor: UIColor.white,
+    ]
+    private let separatorBgColor = UIColor(rgb: 0xC7CBD3)
+    private let margin: CGFloat = 10
+
+    // MARK:- Private variables
+    private var currentSuggestions: [String] = []
+
+    private var currentQuery: String = "" {
+        didSet {
+            if currentQuery.isEmpty {
+                currentSuggestions.removeAll()
+                clearSuggestions()
+            }
+        }
+    }
+
+    // MARK: - Public API
+    func displaySuggestions(query: String, suggestions: [String]) -> Bool {
+        currentQuery = query
+        var suggestionsShown = false
+
+        guard shouldShowSuggestions(query: query, suggestions: suggestions) else {
+            updateSuggestions(suggestions)
+            return false
+        }
+
+        self.clearSuggestions()
+        currentSuggestions = suggestions
+
+        var index = 0
+        var x: CGFloat = margin
+        var difference: CGFloat = 0
+        var offset: CGFloat = 0
+        var displayedSuggestions = [(String, CGFloat)]()
+        let maxSuggestionsCount = getMaxSuggestionsCount()
+
+        // Calcuate extra space after the last suggesion
+        for suggestion in suggestions {
+            if suggestion.trim() == query.trim() {
+                continue
+            }
+            let suggestionWidth = getWidth(suggestion)
+            // show Max N suggestions which does not exceed screen width
+            if x + suggestionWidth > self.frame.width || displayedSuggestions.count == maxSuggestionsCount {
+                break
+            }
+            // increment step
+            x = x + suggestionWidth + 2*margin + 1
+            index = index + 1
+            displayedSuggestions.append((suggestion, suggestionWidth))
+        }
+
+        // distribute the extra space evenly on all suggestions
+        difference = self.frame.width - x
+        offset = round(difference/CGFloat(index))
+
+        // draw the suggestions inside the view
+        x = margin
+        index = 0
+        for (suggestion, width) in displayedSuggestions {
+            let suggestionWidth = width + offset
+            // Adding vertical separator between suggestions
+            if index > 0 {
+                let verticalSeparator = createVerticalSeparator(x)
+                self.addSubview(verticalSeparator)
+            }
+            // Adding the suggestion button
+            let suggestionButton = createSuggestionButton(x, index: index, suggestion: suggestion, suggestionWidth: suggestionWidth)
+            self.addSubview(suggestionButton)
+
+            // increment step
+            x = x + suggestionWidth + 2*margin + 1
+            index = index + 1
+            suggestionsShown = true
+        }
+        return suggestionsShown
+    }
+
+    // MARK:- Helper methods
+    private func clearSuggestions() {
+        let subViews = self.subviews
+        for subView in subViews {
+            subView.removeFromSuperview()
+        }
+    }
+
+    private func shouldShowSuggestions(query: String, suggestions: [String]) -> Bool {
+        return currentQuery == query && suggestions.count > 0
+    }
+
+    private func updateSuggestions(_ suggestions: [String]) {
+        currentSuggestions = suggestions
+    }
+
+    private func getMaxSuggestionsCount() -> Int {
+        switch traitCollection.horizontalSizeClass {
+        case .regular:
+            return 5
+        default:
+            return 3
+        }
+    }
+
+    private func getWidth(_ suggestion: String) -> CGFloat {
+        let sizeOfString = (suggestion as NSString).size(withAttributes: boldFontAttributes)
+        return sizeOfString.width + 5
+    }
+
+    private func createVerticalSeparator(_ x: CGFloat) -> UIView {
+        let verticalSeparator = UIView()
+        verticalSeparator.frame = CGRect(x: x-11, y: 0, width: 1, height: self.frame.height)
+        verticalSeparator.backgroundColor = separatorBgColor
+        return verticalSeparator
+    }
+
+    private func createSuggestionButton(_ x: CGFloat, index: Int, suggestion: String, suggestionWidth: CGFloat) -> UIButton {
+        let button = UIButton(type: .custom)
+        let suggestionTitle = getTitle(suggestion)
+        button.setAttributedTitle(suggestionTitle, for: UIControl.State())
+        button.frame = CGRect(x: x, y: 0, width: suggestionWidth, height: self.frame.height)
+        button.addTarget(self, action: #selector(selectSuggestion(_:)), for: .touchUpInside)
+        button.tag = index
+        return button
+    }
+
+    private func getTitle(_ suggestion: String) -> NSAttributedString {
+        let prefix = currentQuery
+        var title: NSMutableAttributedString!
+
+        if let range = suggestion.range(of: prefix), range.lowerBound == suggestion.startIndex {
+            title = NSMutableAttributedString(string: prefix, attributes: normalFontAttributes)
+            var suffix = suggestion
+            suffix.replaceSubrange(range, with: "")
+            title.append(NSAttributedString(string: suffix, attributes: boldFontAttributes))
+        } else {
+            title = NSMutableAttributedString(string: suggestion, attributes: boldFontAttributes)
+        }
+        return title
+    }
+
+    @objc private func selectSuggestion(_ button: UIButton) {
+        guard let suggestion = button.titleLabel?.text else {
+            return
+        }
+        onSuggestionTapped?(suggestion)
+    }
+}
