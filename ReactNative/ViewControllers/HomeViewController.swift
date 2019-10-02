a1de5c36597e4802b18af8aea0e6d38138a39e3c
Daniel Jilg <winsmith@winsmith.de>
2019-10-02 14:42:50 +0200

    Add documentation to HomeViewController

--- ReactNative/ViewControllers/HomeViewController.swift
+++ ReactNative/ViewControllers/HomeViewController.swift
@@ -11,20 +11,24 @@ import React
 import Shared
 import Storage
 
+/// Shows the New Tab view, including Pinned Sites and Top Sites
 class HomeViewController: UIViewController {
+    // MARK: Properties
     weak var homePanelDelegate: HomePanelDelegate?
 
     fileprivate let profile: Profile
 
+    // MARK: - Initialization
     init(profile: Profile) {
         self.profile = profile
         super.init(nibName: nil, bundle: nil)
     }
 
     required init?(coder aDecoder: NSCoder) {
-        fatalError("init(coder:) has not been implemented")
+        fatalError("init(coder:) has not been implemented, use init(profile:) instead.")
     }
 
+    // MARK: - View Lifecycle
     override func viewDidLoad() {
         super.viewDidLoad()
 
@@ -41,6 +45,9 @@ class HomeViewController: UIViewController {
         }
     }
 
+    // MARK: - API
+
+    // MARK: - Private Implementation
     private func addHomeView(speedDials: [Site], pinnedSites: [Site]) {
         func toDial(site: Site) -> [String: String] {
             return [
@@ -68,6 +75,7 @@ class HomeViewController: UIViewController {
     }
 }
 
+// MARK: - HomeViewControllerProtocol
 extension HomeViewController: HomeViewControllerProtocol {
     func applyTheme() {
         view.backgroundColor = UIColor.theme.browser.background
