d8185c99108262d9f85ec9800591c9b720903438
Shahen Antonyan <shahen@cliqz.com>
2019-10-10 13:35:05 +0400

    Fixed tests build.

---
+++ ClientTests/ProfileTest.swift
@@ -0,0 +1,24 @@
+/* This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at http://mozilla.org/MPL/2.0/. */
+
+@testable import Client
+import Foundation
+import Shared
+import Storage
+
+import XCTest
+
+/*
+ * A base test type for tests that need a profile.
+ */
+class ProfileTest: XCTestCase {
+    func withTestProfile(_ callback: (_ profile: Client.Profile) -> Void) {
+        let profile = MockProfile(databasePrefix: "profile-test")
+        profile._reopen()
+        callback(profile)
+        profile._shutdown()
+    }
+
+}
+
