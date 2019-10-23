88a08ee6d796915543714ffcc5d97e4ec94be918
Garvan Keeley <garvankeeley+github@gmail.com>
2019-10-23 16:57:21 -0400

    Fix #5610 - Nordstrom not loading (#5651)
    The history state helper monkey patches the history push state, but our
    override throws a DataCloneError when copying the url. This breaks
    pushState behaviour. The good news is that we no longer need this hack,
    we get URL change KVO events when pushState nav happens. Tested on
    youtube.com and a few other sites.

--- Client/Frontend/Browser/BrowserViewController.swift
+++ Client/Frontend/Browser/BrowserViewController.swift
@@ -1664,10 +1664,6 @@ extension BrowserViewController: TabDelegate {
 
         tab.addContentScript(LocalRequestHelper(), name: LocalRequestHelper.name())
 
-        let historyStateHelper = HistoryStateHelper(tab: tab)
-        historyStateHelper.delegate = self
-        tab.addContentScript(historyStateHelper, name: HistoryStateHelper.name())
-
         let trampoline = TrampolineTabContentScript(tab: tab)
         trampoline.delegate = self
         tab.addContentScript(trampoline, name: TrampolineTabContentScript.name())
@@ -2217,22 +2213,6 @@ extension BrowserViewController {
     }
 }
 
-extension BrowserViewController: HistoryStateHelperDelegate {
-
-    // This gets called when a website invokes the `history.pushState()` or
-    // `history.replaceState()` web API such as on single-page web apps. It
-    // also occurs during our session restore process, so we be sure to avoid
-    // re-saving the tab manager changes when this happens.
-    func historyStateHelper(_ historyStateHelper: HistoryStateHelper, didPushOrReplaceStateInTab tab: Tab) {
-        navigateInTab(tab: tab, webViewStatus: .finishedNavigation)
-
-        if let url = tab.url, let internalUrl = InternalURL(url), internalUrl.isSessionRestore {
-            return
-        }
-
-        tabManager.storeChanges()
-    }
-}
 
 extension BrowserViewController: KeyboardHelperDelegate {
     func keyboardHelper(_ keyboardHelper: KeyboardHelper, keyboardWillShowWithState state: KeyboardState) {
