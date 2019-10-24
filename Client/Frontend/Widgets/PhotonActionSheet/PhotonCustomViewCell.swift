900a68f758f59b463f74b9f1877ba419fddd157e
Daniel Jilg <winsmith@winsmith.de>
2019-10-24 17:25:38 +0200

    Include Privacy Dashboard (#332)
    * Tracker Info View
    
    * Localization of Tracker Type Names
    
    * Show base Doman and LInk to Report
    
    * Make Labels Multiline
    
    * Pass blocker status to PrivacyIndicator

---
+++ Client/Frontend/Widgets/PhotonActionSheet/PhotonCustomViewCell.swift
@@ -0,0 +1,31 @@
+//
+// Copyright (c) 2017-2019 Cliqz GmbH. All rights reserved.
+//
+// This Source Code Form is subject to the terms of the Mozilla Public
+// License, v. 2.0. If a copy of the MPL was not distributed with this
+// file, You can obtain one at https://mozilla.org/MPL/2.0/.
+//
+
+import UIKit
+
+class PhotonCustomViewCell: UITableViewCell {
+    var customView: UIView? {
+        didSet {
+            backgroundColor = UIColor.clear
+
+            oldValue?.removeFromSuperview()
+            guard let newCustomView = customView else { return }
+
+            newCustomView.tintColor = tintColor
+            contentView.addSubview(newCustomView)
+            newCustomView.snp.makeConstraints { make in
+                make.top.equalToSuperview().offset(10)
+                make.bottom.equalToSuperview().offset(-10)
+                make.left.equalToSuperview().offset(16)
+                make.right.equalToSuperview().offset(-16)
+            }
+        }
+    }
+
+    override var tintColor: UIColor! { didSet { customView?.tintColor = tintColor }}
+}
