cc6c58d2f02e5553cdad8661607e2d2bf4226914
Callie Dunn <q2r5@live.com>
2019-11-06 13:30:04 -0500

    Fix #5707: User-Agent request header on iOS 12 shows iOS 13 (#5709)

--- Shared/UserAgent.swift
+++ Shared/UserAgent.swift
@@ -47,7 +47,7 @@ open class UserAgent {
     }
 
     public static func mobileUserAgent() -> String {
-        return "Mozilla/5.0 (iPhone; CPU iPhone OS 12_1_2 like Mac OS X) AppleWebKit/605.1.15 (KHTML, like Gecko) Mobile/16C101"
+        return "Mozilla/5.0 (\(UIDevice.current.model); CPU OS \(UIDevice.current.systemVersion.replacingOccurrences(of: ".", with: "_")) like Mac OS X) AppleWebKit/605.1.15 (KHTML, like Gecko) FxiOS/\(AppInfo.appVersion) Mobile/15E148 Safari/605.1.15"
     }
 
     public static func oppositeUserAgent() -> String {
