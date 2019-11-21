ff86c37ab1c8e07e84fbe3065727b0d6085a7df9
Daniel Jilg <winsmith@winsmith.de>
2019-11-21 14:19:52 +0100

    Include Swiftgen

---
+++ Shared/Localization/BartyCrouch.swift
@@ -0,0 +1,27 @@
+//
+//  This file is required in order for the `transform` task of the translation helper tool BartyCrouch to work.
+//  See here for more details: https://github.com/Flinesoft/BartyCrouch
+//
+
+import Foundation
+
+enum BartyCrouch {
+    enum SupportedLanguage: String {
+        case english = "en"
+        case german = "de"
+    }
+
+    static func translate(key: String, translations: [SupportedLanguage: String], comment: String? = nil) -> String {
+        let typeName = String(describing: BartyCrouch.self)
+        let methodName = #function
+
+        print(
+            "Warning: [BartyCrouch]",
+            "Untransformed \(typeName).\(methodName) method call found with key '\(key)' and base translations '\(translations)'.",
+            "Please ensure that BartyCrouch is installed and configured correctly."
+        )
+
+        // fall back in case something goes wrong with BartyCrouch transformation
+        return "BC: TRANSFORMATION FAILED!"
+    }
+}
