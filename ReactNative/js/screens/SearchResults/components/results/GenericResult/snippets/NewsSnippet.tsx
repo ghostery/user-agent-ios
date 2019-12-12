bc86365e9170f34a577dfedf9ea51a35e5a36292
Krzysztof Jan Modras <krzysztof@cliqz.com>
2019-12-12 20:26:19 +0100

    News and Weather Snippet styles (#599)

--- ReactNative/js/screens/SearchResults/components/results/GenericResult/snippets/NewsSnippet.tsx
+++ ReactNative/js/screens/SearchResults/components/results/GenericResult/snippets/NewsSnippet.tsx
@@ -14,23 +14,36 @@ import { withTheme } from '../../../../../../contexts/theme';
 
 const styles = StyleSheet.create({
   container: {
+    marginTop: 10,
   },
 });
 
 const date2text = (date: Date) => moment(date).fromNow();
 
+const Separator = (width: number) => <View style={{ width }} />;
+const ListHeader = Separator.bind(null, 37);
+const ListFooter = Separator.bind(null, 7);
+const ListSeparator = Separator.bind(null, 10);
+
 const Snippet = ({ theme, news, openLink }: { theme: any, news: any, openLink: (url: string, type: string) => void }) => {
   const onPressCall = useCallback((url: string) => openLink(url, 'news'), [openLink]);
   return (
     <View style={styles.container}>
-      <NewsSnippet data={news} date2text={date2text} onPress={onPressCall} styles={{
-        itemTitle: {
-          color: theme.textColor,
-        },
-        itemImageCaptionText: {
-          color: theme.descriptionColor,
-        },
-      }}/>
+      <NewsSnippet
+        data={news}
+        date2text={date2text}
+        ListHeader={ListHeader}
+        ListFooter={ListFooter}
+        ListSeparator={ListSeparator}
+        onPress={onPressCall} styles={{
+          itemTitle: {
+            color: theme.textColor,
+          },
+          itemImageCaptionText: {
+            color: theme.descriptionColor,
+          },
+        }}
+      />
     </View>
   );
 };
