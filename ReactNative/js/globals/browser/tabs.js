ff0e92360e9a4d035fab6722abaf3d19574012d9
Krzysztof Jan Modras <chrmod@chrmod.net>
2020-01-02 12:21:39 +0100

    proper filtering for tabs.query

--- ReactNative/js/globals/browser/tabs.js
+++ ReactNative/js/globals/browser/tabs.js
@@ -1,6 +1,7 @@
 import { NativeModules } from 'react-native';
+import globToRegExp from 'glob-to-regexp';
 
-const tabs = {
+const TabsAPI = {
   onCreated: {
     addListener() {},
     removeListener() {},
@@ -17,15 +18,49 @@ const tabs = {
     addListener() {},
     removeListener() {},
   },
-  async query() {
-    console.warn("XXXX")
-    return NativeModules.BrowserTabs.query();
+  async query(queryInfo) {
+    if (!queryInfo) {
+      throw new Error('missing queryInfo');
+    }
+
+    const allTabs = await NativeModules.BrowserTabs.query();
+
+    return allTabs.filter(tab => {
+      if (typeof queryInfo.active === 'boolean') {
+        if (tab.active !== queryInfo.active) {
+          return false;
+        }
+      }
+
+      if (typeof queryInfo.title === 'string') {
+        const titleRegex = globToRegExp(queryInfo.title);
+        if (!titleRegex.test(tab.title)) {
+          return false;
+        }
+      }
+
+      if (typeof queryInfo.url === 'string') {
+        const urlRegex = globToRegExp(queryInfo.url);
+        if (!urlRegex.test(tab.url)) {
+          return false;
+        }
+      } else if (Array.isArray(queryInfo.url)) {
+        if (
+          !queryInfo.url.some(url => {
+            const urlRegex = globToRegExp(url);
+            if (urlRegex.test(tab.url)) {
+              return true;
+            }
+            return false;
+          })
+        ) {
+          return false;
+        }
+      }
+
+      return true;
+    });
   },
 };
 
-tabs.query().then((x) => {
-  console.warn("XXXX1111");
-  console.warn(x)
-})
-
-export default tabs;
+export default TabsAPI;
