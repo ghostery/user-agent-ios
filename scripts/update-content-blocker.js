fef9278e97eb8874e9ecb573320a50d60f31cc55
Krzysztof Jan Modras <chrmod@chrmod.net>
2020-02-05 15:34:41 +0100

    Stronger cookie popup blocking rules - fix #708

--- scripts/update-content-blocker.js
+++ scripts/update-content-blocker.js
@@ -1,35 +1,62 @@
-'use strict';
-
 const fs = require('fs');
-const fetch = require("node-fetch");
+const fetch = require('node-fetch');
 
-(async function () {
+(async () => {
   // Ad rules
-  const adList = await (await fetch("https://cdn.cliqz.com/adblocker/configs/safari-ads/allowed-lists.json")).json();
+  const adList = await (await fetch(
+    'https://cdn.cliqz.com/adblocker/configs/safari-ads/allowed-lists.json',
+  )).json();
 
   // Ad Network rules
   const adNetworkRulesUrl = adList.safari.network;
   const adNetworkRules = await (await fetch(adNetworkRulesUrl)).text();
-  fs.writeFileSync('content-blocker-lib-ios/Lists/safari-ads-network.json', adNetworkRules);
+  fs.writeFileSync(
+    'content-blocker-lib-ios/Lists/safari-ads-network.json',
+    adNetworkRules,
+  );
 
   // Ads Cosmetic rules
   const adCosmeticRulesUrl = adList.safari.cosmetic;
   const adCosmeticRules = await (await fetch(adCosmeticRulesUrl)).text();
-  fs.writeFileSync('content-blocker-lib-ios/Lists/safari-ads-cosmetic.json', adCosmeticRules);
+  fs.writeFileSync(
+    'content-blocker-lib-ios/Lists/safari-ads-cosmetic.json',
+    adCosmeticRules,
+  );
 
   // Tracking rules
-  const trackingList = await (await fetch("https://cdn.cliqz.com/adblocker/configs/safari-tracking/allowed-lists.json")).json();
+  const trackingList = await (await fetch(
+    'https://cdn.cliqz.com/adblocker/configs/safari-tracking/allowed-lists.json',
+  )).json();
 
   // Tracking Network rules
   const trackingNetworkRulesUrl = trackingList.safari.network;
-  const trackingNetworkRules = await (await fetch(trackingNetworkRulesUrl)).text();
-  fs.writeFileSync('content-blocker-lib-ios/Lists/safari-tracking-network.json', trackingNetworkRules);
+  const trackingNetworkRules = await (await fetch(
+    trackingNetworkRulesUrl,
+  )).text();
+  fs.writeFileSync(
+    'content-blocker-lib-ios/Lists/safari-tracking-network.json',
+    trackingNetworkRules,
+  );
 
   // Popups rules
-  const popupsList = await (await fetch("https://cdn.cliqz.com/adblocker/configs/safari-autoconsent/allowed-lists.json")).json();
+  const popupsList = await (await fetch(
+    'https://cdn.cliqz.com/adblocker/configs/safari-cookiemonster/allowed-lists.json',
+  )).json();
 
   // Popup Cosmetic rules
   const popupsCosmeticRulesUrl = popupsList.safari.cosmetic;
-  const popupsCosmeticRules = await (await fetch(popupsCosmeticRulesUrl)).text();
-  fs.writeFileSync('content-blocker-lib-ios/Lists/safari-popups-cosmetic.json', popupsCosmeticRules);
+  const popupsCosmeticRules = await (await fetch(
+    popupsCosmeticRulesUrl,
+  )).text();
+  fs.writeFileSync(
+    'content-blocker-lib-ios/Lists/safari-popups-cosmetic.json',
+    popupsCosmeticRules,
+  );
+  // Popup Networkd rules
+  const popupsNetworkRulesUrl = popupsList.safari.Network;
+  const popupsNetworkRules = await (await fetch(popupsNetworkRulesUrl)).text();
+  fs.writeFileSync(
+    'content-blocker-lib-ios/Lists/safari-popups-network.json',
+    popupsNetworkRules,
+  );
 })();
