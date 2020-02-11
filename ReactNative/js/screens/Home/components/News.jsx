ghostery/user-agent-ios

commit ac4b34449cb5ee677570b521518369fbc88d82da

Remove dependecy on BrowserCore's mobile-cards

ReactNative/js/screens/SearchResults/components/Results.jsx
@@ -12,7 +12,6 @@ import ResultList from './ResultList';
 import SpeedDial from '../../../components/SpeedDial';
 import NativeDrawable from '../../../components/NativeDrawable';
 import { withTheme } from '../../../contexts/theme';
-import CliqzProvider from '../../../contexts/cliqz';
 import t from '../../../services/i18n';
 import { resultTitleFontSize } from '../styles';
 
@@ -146,6 +145,18 @@ function groupBy(arr, n) {
   return group;
 }
 
+function handleAutocompletion(url = '', query = '') {
+  const trimmedUrl = url.replace(/http([s]?):\/\/(www.)?/, '').toLowerCase();
+  const searchLower = query.toLowerCase();
+  if (trimmedUrl.startsWith(searchLower)) {
+    NativeModules.AutoCompletion.autoComplete(trimmedUrl);
+  } else {
+    NativeModules.AutoCompletion.autoComplete(query);
+  }
+}
+
+const hideKeyboard = () => NativeModules.BrowserActions.hideKeyboard();
+
 class Results extends React.Component {
   constructor(props) {
     super(props);
@@ -168,11 +179,12 @@ class Results extends React.Component {
   }
 
   openSearchEngineResultsPage = async (searchEngine, query, index) => {
-    const { results = {}, cliqz } = this.props;
+    const { results = {}, searchModule } = this.props;
     const meta = results.meta || {};
     const { favIconUrl: url } = searchEngine;
 
-    await cliqz.search.reportSelection(
+    await searchModule.action(
+      'reportSelection',
       {
         action: 'click',
         elementName: 'icon',
@@ -196,14 +208,25 @@ class Results extends React.Component {
         contextId: 'mobile-cards',
       },
     );
+
     browser.search.search({
       query,
       engine: searchEngine.name,
     });
   };
 
+  reportHighlight = () => {
+    const { searchModule } = this.props;
+    searchModule.reportHighlight();
+  };
+
   render() {
-    const { results: _results, query, theme: _theme, cliqz } = this.props;
+    const {
+      results: _results,
+      query,
+      theme: _theme,
+      searchModule,
+    } = this.props;
     const {
       results: allResults,
       suggestions,
@@ -222,115 +245,110 @@ class Results extends React.Component {
     if (results[0]) {
       const { friendlyUrl, text } = results[0];
       if (friendlyUrl && text) {
-        cliqz.mobileCards.handleAutocompletion(friendlyUrl, text);
+        handleAutocompletion(friendlyUrl, text);
       }
     }
 
     return (
       <View style={styles.container}>
-        <CliqzProvider.Provider value={cliqz}>
-          <ScrollView
-            bounces
-            ref={this.scrollRef}
-            showsVerticalScrollIndicator={false}
-            keyboardDismissMode="on-drag"
-            keyboardShouldPersistTaps="handled"
-            onTouchStart={() => cliqz.mobileCards.hideKeyboard()}
-            onScrollEndDrag={() => cliqz.search.reportHighlight()}
+        <ScrollView
+          bounces
+          ref={this.scrollRef}
+          showsVerticalScrollIndicator={false}
+          keyboardDismissMode="on-drag"
+          keyboardShouldPersistTaps="handled"
+          onTouchStart={hideKeyboard}
+          onScrollEndDrag={this.reportHighlight}
+        >
+          <View style={styles.bouncer} />
+          <View
+            accessible={false}
+            accessibilityLabel="search-results"
+            style={styles.searchUI}
           >
-            <View style={styles.bouncer} />
-            <View
-              accessible={false}
-              accessibilityLabel="search-results"
-              style={styles.searchUI}
-            >
-              <ResultList
-                results={results}
-                meta={meta}
-                style={styles.cardListStyle}
-                header={<View />}
-                separator={<View style={styles.separator} />}
-                footer={<View />}
-              />
-            </View>
-            <>
-              {results.length === 0 && (
-                <View style={styles.noResults}>
-                  <Text style={styles.noResultsText}>
-                    {t('search_no_results')}
-                  </Text>
-                </View>
-              )}
-
-              <View style={styles.footer}>
-                <TouchableWithoutFeedback
-                  onPress={() =>
-                    this.openSearchEngineResultsPage(
-                      { name: 'Cliqz' },
-                      query,
-                      0,
-                    )
-                  }
-                >
-                  <View style={styles.showMoreButtonWrapper}>
-                    <View style={styles.showMoreButton}>
-                      <NativeDrawable
-                        style={styles.footerIcon}
-                        source="nav-menu"
-                        color="#ffffff"
-                      />
-                      <Text style={styles.footerText} allowFontScaling={false}>
-                        {t('search_footer')}
-                      </Text>
-                    </View>
-                  </View>
-                </TouchableWithoutFeedback>
-              </View>
-
-              <View style={styles.searchEnginesHeader}>
-                <Text style={styles.searchEnginesHeaderText}>
-                  {t('search_alternative_search_engines_info')}
+            <ResultList
+              results={results}
+              meta={meta}
+              style={styles.cardListStyle}
+              header={<View />}
+              separator={<View style={styles.separator} />}
+              footer={<View />}
+              searchModule={searchModule}
+            />
+          </View>
+          <>
+            {results.length === 0 && (
+              <View style={styles.noResults}>
+                <Text style={styles.noResultsText}>
+                  {t('search_no_results')}
                 </Text>
               </View>
-              <View style={styles.searchEnginesContainer}>
-                {groupBy(searchEngines, 3).map(
-                  (searchEnginesGroup, groupIndex) => (
-                    <View
-                      style={styles.searchEnginesGroupContainer}
-                      key={searchEnginesGroup.map(e => e.name).join('')}
-                    >
-                      {searchEnginesGroup.map((searchEngine, engineIndex) => (
-                        <SpeedDial
-                          key={searchEngine.name}
-                          styles={{
-                            label: {
-                              color: 'white',
-                            },
-                            circle: {
-                              borderColor: `${_theme.separatorColor}44`,
-                            },
-                          }}
-                          speedDial={{
-                            pinned: false,
-                            url: searchEngine.favIconUrl,
-                          }}
-                          onPress={() =>
-                            this.openSearchEngineResultsPage(
-                              searchEngine,
-                              query,
-                              // index 0 is "show more results" Cliqz link
-                              1 + groupIndex * 3 + engineIndex,
-                            )
-                          }
-                        />
-                      ))}
-                    </View>
-                  ),
-                )}
-              </View>
-            </>
-          </ScrollView>
-        </CliqzProvider.Provider>
+            )}
+
+            <View style={styles.footer}>
+              <TouchableWithoutFeedback
+                onPress={() =>
+                  this.openSearchEngineResultsPage({ name: 'Cliqz' }, query, 0)
+                }
+              >
+                <View style={styles.showMoreButtonWrapper}>
+                  <View style={styles.showMoreButton}>
+                    <NativeDrawable
+                      style={styles.footerIcon}
+                      source="nav-menu"
+                      color="#ffffff"
+                    />
+                    <Text style={styles.footerText} allowFontScaling={false}>
+                      {t('search_footer')}
+                    </Text>
+                  </View>
+                </View>
+              </TouchableWithoutFeedback>
+            </View>
+
+            <View style={styles.searchEnginesHeader}>
+              <Text style={styles.searchEnginesHeaderText}>
+                {t('search_alternative_search_engines_info')}
+              </Text>
+            </View>
+            <View style={styles.searchEnginesContainer}>
+              {groupBy(searchEngines, 3).map(
+                (searchEnginesGroup, groupIndex) => (
+                  <View
+                    style={styles.searchEnginesGroupContainer}
+                    key={searchEnginesGroup.map(e => e.name).join('')}
+                  >
+                    {searchEnginesGroup.map((searchEngine, engineIndex) => (
+                      <SpeedDial
+                        key={searchEngine.name}
+                        styles={{
+                          label: {
+                            color: 'white',
+                          },
+                          circle: {
+                            borderColor: `${_theme.separatorColor}44`,
+                          },
+                        }}
+                        speedDial={{
+                          pinned: false,
+                          url: searchEngine.favIconUrl,
+                        }}
+                        onPress={() =>
+                          this.openSearchEngineResultsPage(
+                            searchEngine,
+                            query,
+                            // index 0 is "show more results" Cliqz link
+                            1 + groupIndex * 3 + engineIndex,
+                          )
+                        }
+                      />
+                    ))}
+                  </View>
+                ),
+              )}
+            </View>
+          </>
+        </ScrollView>
       </View>
     );
   }

ReactNative/js/screens/SearchResults/index.jsx
@@ -1,7 +1,6 @@
 /* eslint-disable react/prop-types */
 import React from 'react';
 import Results from './components/Results';
-import Cliqz from './services/cliqz-wrapper';
 
 export default class SearchResults extends React.Component {
   constructor(props) {
@@ -13,8 +12,6 @@ export default class SearchResults extends React.Component {
     this.searchRef = React.createRef();
     this.onAction = this.onAction.bind(this);
     this.onResults = this.onResults.bind(this);
-    const { bridgeManager } = this.props;
-    this.cliqz = new Cliqz(bridgeManager.inject);
   }
 
   // eslint-disable-next-line react/no-deprecated
@@ -52,12 +49,10 @@ export default class SearchResults extends React.Component {
 
   render() {
     const { query, results } = this.state;
+    const { searchModule } = this.props;
 
-    // eslint-disable-next-line prettier/prettier
-    return <Results
-      results={results}
-      query={query}
-      cliqz={this.cliqz}
-    />;
+    return (
+      <Results results={results} query={query} searchModule={searchModule} />
+    );
   }
 }

ReactNative/NativeModules/BrowserActions.m
@@ -10,7 +10,7 @@
 
 @interface RCT_EXTERN_MODULE(BrowserActions, NSObject)
 
-RCT_EXTERN_METHOD(openLink:(nonnull NSString *)url query:(nonnull NSString *)query isSearchEngine:(BOOL)isSearchEngine)
+RCT_EXTERN_METHOD(openLink:(nonnull NSString *)url query:(nonnull NSString *)query)
 RCT_EXTERN_METHOD(searchHistory:(nonnull NSString *)query callback:(RCTResponseSenderBlock))
 RCT_EXTERN_METHOD(hideKeyboard)
 RCT_EXTERN_METHOD(showQuerySuggestions:(nullable NSString *)query suggestions:(nullable NSArray*)suggestions)

ReactNative/NativeModules/BrowserActions.swift
@@ -13,8 +13,8 @@ let HideKeyboardSearchNotification = NSNotification.Name(rawValue: "Search:hideK
 
 @objc(BrowserActions)
 class BrowserActions: NSObject, NativeModuleBase {
-    @objc(openLink:query:isSearchEngine:)
-    public func openLink(url_str: NSString, query: NSString, isSearchEngine: Bool) {
+    @objc(openLink:query:)
+    public func openLink(url_str: NSString, query: NSString) {
         self.withAppDelegate { appDel in
             appDel.useCases.openLink.openLink(urlString: url_str as String, query: query as String)
         }

ReactNative/js/contexts/cliqz.js
@@ -1,18 +1,0 @@
-import React from 'react';
-
-const CliqzContext = React.createContext();
-
-export function withCliqz(Component) {
-  return function CliqzComponent(props) {
-    /* eslint-disable react/jsx-props-no-spreading */
-    return (
-      // eslint-disable-next-line react/jsx-filename-extension
-      <CliqzContext.Consumer>
-        {cliqz => <Component {...props} cliqz={cliqz} />}
-      </CliqzContext.Consumer>
-    );
-  };
-  /* eslint-enable react/jsx-props-no-spreading */
-}
-
-export default CliqzContext;

ReactNative/js/screens/SearchResults/components/ResultList.jsx
@@ -11,7 +11,6 @@ import React from 'react';
 import { View, StyleSheet, NativeModules } from 'react-native';
 import GenericResult from './results/GenericResult';
 import WeatherSnippet from './results/WeatherResult';
-import { withCliqz } from '../../../contexts/cliqz';
 import { isSwitchToTab } from './helpers';
 
 const styles = StyleSheet.create({
@@ -22,8 +21,10 @@ const styles = StyleSheet.create({
 
 const onLongPress = ({ url, title, isHistory, query } = {}) =>
   NativeModules.ContextMenu.result(url, title, isHistory, query);
+const openLink = (url, query) =>
+  NativeModules.BrowserActions.openLink(url, query);
 
-class CardList extends React.PureComponent {
+export default class CardList extends React.PureComponent {
   constructor(props) {
     super(props);
     this.lastText = '';
@@ -50,14 +51,19 @@ class CardList extends React.PureComponent {
     return selection;
   };
 
-  openLink = (result, url, elementName = '') => {
-    const { cliqz } = this.props;
+  openLink = async (result, url, elementName = '') => {
+    const { searchModule } = this.props;
     const selection = this.getSelection(result, url, elementName);
     let actionUrl = url;
     if (isSwitchToTab(result)) {
       actionUrl = `moz-action:switchtab,${JSON.stringify({ url })}`;
     }
-    cliqz.mobileCards.openLink(actionUrl, selection);
+
+    await searchModule.action('reportSelection', selection, {
+      contextId: 'mobile-cards',
+    });
+
+    openLink(actionUrl, selection.query);
   };
 
   getComponent = ({ item, index }) => {
@@ -113,5 +119,3 @@ class CardList extends React.PureComponent {
     );
   }
 }
-
-export default withCliqz(CardList);

ReactNative/js/screens/SearchResults/services/cliqz-wrapper.js
@@ -1,35 +1,0 @@
-const callAction = (inject, module, action, ...args) =>
-  inject.module(module).action(action, ...args);
-const createModuleWrapper = (inject, module, actions) =>
-  actions.reduce(
-    (all, action) => ({
-      ...all,
-      [action]: callAction.bind(null, inject, module, action),
-    }),
-    {},
-  );
-
-export default class Cliqz {
-  constructor(inject) {
-    this.mobileCards = createModuleWrapper(inject, 'mobile-cards', [
-      'openLink',
-      'callNumber',
-      'openMap',
-      'hideKeyboard',
-      'sendUIReadySignal',
-      'handleAutocompletion',
-      'getConfig',
-      'getTrackerDetails',
-    ]);
-
-    this.core = createModuleWrapper(inject, 'core', []);
-    this.search = createModuleWrapper(inject, 'search', [
-      'getSnippet',
-      'reportHighlight',
-      'reportSelection',
-    ]);
-    this.geolocation = createModuleWrapper(inject, 'geolocation', [
-      'updateGeoLocation',
-    ]);
-  }
-}

index.js
@@ -58,6 +58,6 @@ bridgeManager.addActionListener(({ module, action, args /* , id */ }) => {
 AppRegistry.setWrapperComponentProvider(ThemeWrapperComponentProvider(bridgeManager));
 AppRegistry.registerComponent('BrowserCore', () => class extends React.Component { render() { return null; }});
 AppRegistry.registerComponent('Home', () => (props) => <Home newsModule={inject.module('news')} {...props} />);
-AppRegistry.registerComponent('SearchResults', () => (props) => <SearchResults bridgeManager={bridgeManager} events={events} {...props} />);
+AppRegistry.registerComponent('SearchResults', () => (props) => <SearchResults searchModule={inject.module('search')} bridgeManager={bridgeManager} events={events} {...props} />);
 AppRegistry.registerComponent('Logo', () => Logo);
 AppRegistry.registerComponent('PrivacyStats', () => (props) => <PrivacyStats insightsModule={inject.module('insights')} {...props} />);

commit b29d8cd244a3ca6000053ae6d2599b4430e62696

Fixing crashes caused by #759

ReactNative/js/screens/Home/components/News.jsx
@@ -34,7 +34,7 @@ const getStyles = theme =>
     },
   });
 
-const openLink = url => NativeModules.BrowserActions.openLink(url, '', false);
+const openLink = url => NativeModules.BrowserActions.openLink(url, '');
 
 const deepEqualNews = (oldNews, news) => {
   return oldNews.every((_, index) => {

ReactNative/js/screens/Home/index.jsx
@@ -13,7 +13,7 @@ import SpeedDial from '../../components/SpeedDial';
 import News from './components/News';
 
 const openSpeedDialLink = speedDial =>
-  NativeModules.BrowserActions.openLink(speedDial.url, '', false);
+  NativeModules.BrowserActions.openLink(speedDial.url, '');
 const longPressSpeedDial = speedDial =>
   NativeModules.ContextMenu.speedDial(speedDial.url, speedDial.pinned || false);
 const hideKeyboard = () => NativeModules.BrowserActions.hideKeyboard();

ReactNative/js/screens/SearchResults/components/Results.jsx
@@ -217,7 +217,7 @@ class Results extends React.Component {
 
   reportHighlight = () => {
     const { searchModule } = this.props;
-    searchModule.reportHighlight();
+    searchModule.action('reportHighlight');
   };
 
   render() {