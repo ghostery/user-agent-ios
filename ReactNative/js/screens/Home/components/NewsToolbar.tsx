7a5415dfff5656640761355603fd2e092c2e40bb
Krzysztof Jan Modras <chrmod@chrmod.net>
2020-03-27 14:19:54 +0100

    News button

---
+++ ReactNative/js/screens/Home/components/NewsToolbar.tsx
@@ -0,0 +1,41 @@
+import React from 'react';
+import { TouchableWithoutFeedback, View, Text, StyleSheet } from 'react-native';
+import NativeDrawable from '../../../components/NativeDrawable';
+
+const styles = StyleSheet.create({
+  wrapper: {
+    width: '100%',
+  },
+  button: {
+    alignSelf: 'flex-end',
+    flexDirection: 'row',
+  },
+  buttonText: {
+    color: 'white',
+    fontSize: 15,
+    marginRight: 5,
+  },
+  buttonIcon: {
+    color: '#ffffff',
+    height: 20,
+    width: 20,
+    transform: [{ rotate: '-90deg' }],
+  },
+});
+
+export default () => {
+  return (
+    <View style={styles.wrapper}>
+      <TouchableWithoutFeedback>
+        <View style={styles.button}>
+          <Text style={styles.buttonText}>News</Text>
+          <NativeDrawable
+            style={styles.buttonIcon}
+            source="nav-back"
+            color={styles.buttonIcon.color}
+          />
+        </View>
+      </TouchableWithoutFeedback>
+    </View>
+  );
+};
