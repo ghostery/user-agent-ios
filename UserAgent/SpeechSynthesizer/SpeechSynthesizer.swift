0840024e193734b457b86c398a22ae996f99482f
Shahen Antonyan <shahenantonyan.ios@gmail.com>
2020-04-07 16:49:13 +0400

    Implemented Speech synthesizer.

---
+++ UserAgent/SpeechSynthesizer/SpeechSynthesizer.swift
@@ -0,0 +1,84 @@
+//
+// Copyright (c) 2017-2019 Cliqz GmbH. All rights reserved.
+//
+// This Source Code Form is subject to the terms of the Mozilla Public
+// License, v. 2.0. If a copy of the MPL was not distributed with this
+// file, You can obtain one at https://mozilla.org/MPL/2.0/.
+//
+
+import AVFoundation
+
+enum SpeechLanguage: String {
+    case de = "de-DE" // German
+    case en_us = "en-US" // English(US)
+    case en_gb = "en-GB" // English(GB)
+    case fr = "fr-CA" // French
+    case it = "it-IT" // Italian
+    case es = "es-ES" // Spanish
+}
+
+protocol SpeechSynthesizerDelegate: class {
+    func speechSynthesizerDidStart()
+    func speechSynthesizerDidPause()
+    func speechSynthesizerDidCancel()
+    func speechSynthesizerDidFinish()
+    func speechSynthesizerDidContinue()
+}
+
+class SpeechSynthesizer: NSObject {
+
+    weak var delegate: SpeechSynthesizerDelegate?
+
+    private let speechSynthesizer = AVSpeechSynthesizer()
+
+    override init() {
+        super.init()
+        self.speechSynthesizer.delegate = self
+    }
+
+    func start(text: String, language: SpeechLanguage) {
+        guard !self.speechSynthesizer.isSpeaking || self.speechSynthesizer.stopSpeaking(at: .immediate) else {
+            return
+        }
+        let utterance = AVSpeechUtterance(string: text)
+        utterance.voice = AVSpeechSynthesisVoice(language: language.rawValue)
+        self.speechSynthesizer.speak(utterance)
+    }
+
+    func pause() {
+        self.speechSynthesizer.pauseSpeaking(at: .word)
+    }
+
+    func resume() {
+        self.speechSynthesizer.continueSpeaking()
+    }
+
+    func stop() {
+        self.speechSynthesizer.stopSpeaking(at: .word)
+    }
+
+}
+
+extension SpeechSynthesizer: AVSpeechSynthesizerDelegate {
+
+    func speechSynthesizer(_ synthesizer: AVSpeechSynthesizer, didStart utterance: AVSpeechUtterance) {
+        self.delegate?.speechSynthesizerDidStart()
+    }
+
+    func speechSynthesizer(_ synthesizer: AVSpeechSynthesizer, didPause utterance: AVSpeechUtterance) {
+        self.delegate?.speechSynthesizerDidPause()
+    }
+
+    func speechSynthesizer(_ synthesizer: AVSpeechSynthesizer, didCancel utterance: AVSpeechUtterance) {
+        self.delegate?.speechSynthesizerDidCancel()
+    }
+
+    func speechSynthesizer(_ synthesizer: AVSpeechSynthesizer, didFinish utterance: AVSpeechUtterance) {
+        self.delegate?.speechSynthesizerDidFinish()
+    }
+
+    func speechSynthesizer(_ synthesizer: AVSpeechSynthesizer, didContinue utterance: AVSpeechUtterance) {
+        self.delegate?.speechSynthesizerDidContinue()
+    }
+
+}
