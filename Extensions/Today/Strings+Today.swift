c0d5753b564c61b8296838921eaf561e27275399
Shahen <shahen@cliqz.com>
2020-05-18 14:38:25 +0400

    Moved Today widget localization under the strings file.

---
+++ Extensions/Today/Strings+Today.swift
@@ -0,0 +1,18 @@
+//
+// Copyright (c) 2017-2020 Cliqz GmbH. All rights reserved.
+//
+// This Source Code Form is subject to the terms of the Mozilla Public
+// License, v. 2.0. If a copy of the MPL was not distributed with this
+// file, You can obtain one at https://mozilla.org/MPL/2.0/.
+//
+
+import Foundation
+
+extension Strings {
+    public struct Today {
+        public static let Reload = NSLocalizedString("Today.Reload", tableName: "Today", comment: "Reload weather data")
+        public static let Configure = NSLocalizedString("Today.Configure", tableName: "Today", comment: "Configure weather widget")
+        public static let Expand = NSLocalizedString("Today.Expand", tableName: "Today", comment: "Show more info")
+        public static let Collapse = NSLocalizedString("Today.Collapse", tableName: "Today", comment: "Show less info")
+    }
+}
