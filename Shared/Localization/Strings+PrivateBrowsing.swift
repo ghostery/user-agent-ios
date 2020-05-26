b93250176c7c21c2eb795d52bcaf5ae5b981f645
Shahen Antonyan <shahenantonyan.ios@gmail.com>
2020-05-26 15:27:59 +0400

    Small fix.

--- Shared/Localization/Strings+PrivateBrowsing.swift
+++ Shared/Localization/Strings+PrivateBrowsing.swift
@@ -12,20 +12,20 @@ import Foundation
 extension Strings {
     public struct ForgetMode {
         public struct ShareExtension {
-            public static var OpenInPrivateTab = NSLocalizedString("ForgetMode.ShareExtension.OpenInPrivateTab", tableName: "PrivateMode", comment: "Action label on share extension to immediately open page in Private.")
+            public static var OpenInPrivateTab = NSLocalizedString("ForgetMode.ShareExtension.OpenInPrivateTab", tableName: "ForgetMode", comment: "Action label on share extension to immediately open page in Private.")
         }
         public struct AutomaticPrivateMode {
-            public static let Title = NSLocalizedString("ForgetMode.AutomaticForgetMode.Title", tableName: "ContextualOnboarding", comment: "Title for Automatic forget mode screen")
-            public static let Description = NSLocalizedString("ForgetMode.AutomaticForgetMode.Description", tableName: "ContextualOnboarding", comment: "Description for Automatic forget mode screen")
+            public static let Title = NSLocalizedString("ForgetMode.AutomaticForgetMode.Title", tableName: "ForgetMode", comment: "Title for Automatic forget mode screen")
+            public static let Description = NSLocalizedString("ForgetMode.AutomaticForgetMode.Description", tableName: "ForgetMode", comment: "Description for Automatic forget mode screen")
         }
         public struct ContextMenu {
-            public static let OpenInNewPrivateTab = NSLocalizedString("ForgetMode.ContextMenu.OpenInNewPrivateTab", comment: "The title for the Open in New Private Tab context menu action for sites in Home Panels")
+            public static let OpenInNewPrivateTab = NSLocalizedString("ForgetMode.ContextMenu.OpenInNewPrivateTab", tableName: "ForgetMode", comment: "The title for the Open in New Private Tab context menu action for sites in Home Panels")
         }
         public struct Hotkeys {
-            public static let PrivateBrowsingModeTitle = NSLocalizedString("ForgetMode.Hotkeys.PrivateBrowsingModeTitle", comment: "Label to switch to private browsing mode")
+            public static let PrivateBrowsingModeTitle = NSLocalizedString("ForgetMode.Hotkeys.PrivateBrowsingModeTitle", tableName: "ForgetMode", comment: "Label to switch to private browsing mode")
         }
         public struct TabTray {
-            public static let SwitchToPBMKeyCodeTitle = NSLocalizedString("ForgetMode.TabTray.SwitchToPBMKeyCodeTitle", comment: "Hardware shortcut switch to the private browsing tab or tab tray. Shown in the Discoverability overlay when the hardware Command Key is held down.")
+            public static let SwitchToPBMKeyCodeTitle = NSLocalizedString("ForgetMode.TabTray.SwitchToPBMKeyCodeTitle", tableName: "ForgetMode", comment: "Hardware shortcut switch to the private browsing tab or tab tray. Shown in the Discoverability overlay when the hardware Command Key is held down.")
         }
     }
     public static let PrivateBrowsingEmptyPrivateTabsTitle = NSLocalizedString("Private Browsing", tableName: "PrivateBrowsing", comment: "Title displayed for when there are no open tabs while in private mode")
