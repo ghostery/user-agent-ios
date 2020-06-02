2d025d7f4b6a467e884288d609d0a0448fa4e7d2
Shahen Antonyan <shahenantonyan.ios@gmail.com>
2020-06-02 16:22:17 +0400

    Moved Branding/Cliqz/Configuration/Subfolders under parent folder.

--- Branding/setup.sh
+++ Branding/setup.sh
@@ -1,58 +1,58 @@
 #!/bin/sh
 # Passing target name as an argument.
-TARGETNAME=$1
-PROJECTPATH=$2
+TARGET_NAME=$1
+PROJECT_PATH=$2
 
-if [ -z "${PROJECTPATH}" ]; then
+if [ -z "${PROJECT_PATH}" ]; then
     echo "Project path is empty!"
     exit 1
 fi
 
-if [ -z "${TARGETNAME}" ]; then
+if [ -z "${TARGET_NAME}" ]; then
     echo "Target name is empty!"
     exit 1
 fi
 
 # Configuration folder path
-CONFIG_PATH=${PROJECTPATH}/Client/Configuration
-BRAND_PATH=${PROJECTPATH}/Branding/${TARGETNAME}/Configuration
+CONFIG_PATH=${PROJECT_PATH}/Client/Configuration
+BRAND_PATH=${PROJECT_PATH}/Branding/${TARGET_NAME}
 
 # Replacing Debug/AdHoc/Release xcconfig files according target
 mkdir -p ${CONFIG_PATH}
-cp -rf ${BRAND_PATH}/${TARGETNAME}Debug.xcconfig ${CONFIG_PATH}/Debug.xcconfig
-cp -rf ${BRAND_PATH}/${TARGETNAME}AdHoc.xcconfig ${CONFIG_PATH}/AdHoc.xcconfig
-cp -rf ${BRAND_PATH}/${TARGETNAME}Release.xcconfig ${CONFIG_PATH}/Release.xcconfig
+cp -rf ${BRAND_PATH}/Configuration/${TARGET_NAME}Debug.xcconfig ${CONFIG_PATH}/Debug.xcconfig
+cp -rf ${BRAND_PATH}/Configuration/${TARGET_NAME}AdHoc.xcconfig ${CONFIG_PATH}/AdHoc.xcconfig
+cp -rf ${BRAND_PATH}/Configuration/${TARGET_NAME}Release.xcconfig ${CONFIG_PATH}/Release.xcconfig
 
 # Entitlments folder path
-SHARETO_ENTITLEMENTS_PATH=${PROJECTPATH}/Extensions/ShareTo/Entitlements
-OPENIN_ENTITLEMENTS_PATH=${PROJECTPATH}/Extensions/OpenIn/Entitlements
-TODAY_ENTITLEMENTS_PATH=${PROJECTPATH}/Extensions/Today/Entitlements
+SHARETO_ENTITLEMENTS_PATH=${PROJECT_PATH}/Extensions/ShareTo/Entitlements
+OPENIN_ENTITLEMENTS_PATH=${PROJECT_PATH}/Extensions/OpenIn/Entitlements
+TODAY_ENTITLEMENTS_PATH=${PROJECT_PATH}/Extensions/Today/Entitlements
 
 # Replacing ShareTo/OpenIn entitlement files according target
 mkdir -p ${SHARETO_ENTITLEMENTS_PATH}
-cp -rf ${BRAND_PATH}/Entitlements/ShareTo/${TARGETNAME}ShareTo.entitlements ${SHARETO_ENTITLEMENTS_PATH}/ShareTo.entitlements
-cp -rf ${BRAND_PATH}/Entitlements/ShareTo/${TARGETNAME}ShareToAdHoc.entitlements ${SHARETO_ENTITLEMENTS_PATH}/ShareToAdHoc.entitlements
-cp -rf ${BRAND_PATH}/Entitlements/ShareTo/${TARGETNAME}ShareToDebug.entitlements ${SHARETO_ENTITLEMENTS_PATH}/ShareToDebug.entitlements
+cp -rf ${BRAND_PATH}/Entitlements/ShareTo/${TARGET_NAME}ShareTo.entitlements ${SHARETO_ENTITLEMENTS_PATH}/ShareTo.entitlements
+cp -rf ${BRAND_PATH}/Entitlements/ShareTo/${TARGET_NAME}ShareToAdHoc.entitlements ${SHARETO_ENTITLEMENTS_PATH}/ShareToAdHoc.entitlements
+cp -rf ${BRAND_PATH}/Entitlements/ShareTo/${TARGET_NAME}ShareToDebug.entitlements ${SHARETO_ENTITLEMENTS_PATH}/ShareToDebug.entitlements
 
 mkdir -p ${OPENIN_ENTITLEMENTS_PATH}
-cp -rf ${BRAND_PATH}/Entitlements/OpenIn/${TARGETNAME}OpenIn.entitlements ${OPENIN_ENTITLEMENTS_PATH}/OpenIn.entitlements
-cp -rf ${BRAND_PATH}/Entitlements/OpenIn/${TARGETNAME}OpenInAdHoc.entitlements ${OPENIN_ENTITLEMENTS_PATH}/OpenInAdHoc.entitlements
-cp -rf ${BRAND_PATH}/Entitlements/OpenIn/${TARGETNAME}OpenInDebug.entitlements ${OPENIN_ENTITLEMENTS_PATH}/OpenInDebug.entitlements
+cp -rf ${BRAND_PATH}/Entitlements/OpenIn/${TARGET_NAME}OpenIn.entitlements ${OPENIN_ENTITLEMENTS_PATH}/OpenIn.entitlements
+cp -rf ${BRAND_PATH}/Entitlements/OpenIn/${TARGET_NAME}OpenInAdHoc.entitlements ${OPENIN_ENTITLEMENTS_PATH}/OpenInAdHoc.entitlements
+cp -rf ${BRAND_PATH}/Entitlements/OpenIn/${TARGET_NAME}OpenInDebug.entitlements ${OPENIN_ENTITLEMENTS_PATH}/OpenInDebug.entitlements
 
 mkdir -p ${TODAY_ENTITLEMENTS_PATH}
-cp -rf ${BRAND_PATH}/Entitlements/Today/${TARGETNAME}Today.entitlements ${TODAY_ENTITLEMENTS_PATH}/Today.entitlements
-cp -rf ${BRAND_PATH}/Entitlements/Today/${TARGETNAME}TodayAdHoc.entitlements ${TODAY_ENTITLEMENTS_PATH}/TodayAdHoc.entitlements
-cp -rf ${BRAND_PATH}/Entitlements/Today/${TARGETNAME}TodayDebug.entitlements ${TODAY_ENTITLEMENTS_PATH}/TodayDebug.entitlements
+cp -rf ${BRAND_PATH}/Entitlements/Today/${TARGET_NAME}Today.entitlements ${TODAY_ENTITLEMENTS_PATH}/Today.entitlements
+cp -rf ${BRAND_PATH}/Entitlements/Today/${TARGET_NAME}TodayAdHoc.entitlements ${TODAY_ENTITLEMENTS_PATH}/TodayAdHoc.entitlements
+cp -rf ${BRAND_PATH}/Entitlements/Today/${TARGET_NAME}TodayDebug.entitlements ${TODAY_ENTITLEMENTS_PATH}/TodayDebug.entitlements
 
 # ShareTo folder path
-SHARE_TO_PATH=${PROJECTPATH}/Extensions/ShareTo
+SHARE_TO_PATH=${PROJECT_PATH}/Extensions/ShareTo
 
 # Replacing ShareTo assets according target
 rm -rf ${SHARE_TO_PATH}/ShareTo.xcassets
 cp -rf ${BRAND_PATH}/Assets/ShareTo.xcassets ${SHARE_TO_PATH}/
 
 # OpenIn folder path
-OPEN_IN_PATH=${PROJECTPATH}/Extensions/OpenIn
+OPEN_IN_PATH=${PROJECT_PATH}/Extensions/OpenIn
 
 # Replacing OpenIn assets according target
 rm -rf ${OPEN_IN_PATH}/OpenIn.xcassets
@@ -65,7 +65,7 @@ cp -rf ${BRAND_PATH}/InfoPlists/OpenIn/de.lproj/InfoPlist.strings ${OPEN_IN_PATH
 cp -rf ${BRAND_PATH}/InfoPlists/OpenIn/en.lproj/InfoPlist.strings ${OPEN_IN_PATH}/en.lproj/InfoPlist.strings
 
 # Today folder path
-TODAY_PATH=${PROJECTPATH}/Extensions/Today
+TODAY_PATH=${PROJECT_PATH}/Extensions/Today
 
 # Replacing OpenIn assets and InfoPlist.strings according target
 mkdir -p ${TODAY_PATH}/de.lproj
@@ -74,7 +74,7 @@ cp -rf ${BRAND_PATH}/InfoPlists/Today/de.lproj/InfoPlist.strings ${TODAY_PATH}/d
 cp -rf ${BRAND_PATH}/InfoPlists/Today/en.lproj/InfoPlist.strings ${TODAY_PATH}/en.lproj/InfoPlist.strings
 
 # Siri folder path
-SIRI_PATH=${PROJECTPATH}/Extensions/Siri
+SIRI_PATH=${PROJECT_PATH}/Extensions/Siri
 
 # Replacing Siri localizations according target
 mkdir -p ${SIRI_PATH}/de.lproj
