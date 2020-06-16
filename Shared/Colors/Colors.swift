4c166ef3fd6fec25f0e333842a27354ef1d0f9a1
Shahen Antonyan <shahenantonyan.ios@gmail.com>
2020-06-16 14:16:08 +0400

    Fixed search and widget colors issue.

---
+++ Shared/Colors/Colors.swift
@@ -0,0 +1,60 @@
+//
+// Copyright (c) 2017-2019 Cliqz GmbH. All rights reserved.
+//
+// This Source Code Form is subject to the terms of the Mozilla Public
+// License, v. 2.0. If a copy of the MPL was not distributed with this
+// file, You can obtain one at https://mozilla.org/MPL/2.0/.
+//
+
+import UIKit
+
+extension UIColor {
+    // Primary Colors
+    public static let Blue = UIColor(named: "Blue")!
+    public static let Black = UIColor(named: "Black")!
+    public static let White = UIColor(named: "White")!
+
+    // Secondary Colors
+    public static let DarkRain = UIColor(named: "DarkRain")!
+    public static let CloudySky = UIColor(named: "CloudySky")!
+    public static let LightSky = UIColor(named: "LightSky")!
+    public static let BrightBlue = UIColor(named: "BrightBlue")!
+    public static let DarkBlue = UIColor(named: "DarkBlue")!
+    public static let LightBlue = UIColor(named: "LightBlue")!
+    public static let LightGrey = UIColor(named: "LightGrey")!
+    public static let Orange = UIColor(named: "Orange")!
+
+    // System Colors
+    public static let defaultBackground = UIColor(named: "Background")!
+    public static let homeBackground = UIColor(named: "HomeScreenBackground")!
+    public static let DarkGrey = UIColor(named: "DarkGrey")!
+    public static let TextHighlightBlue = UIColor(named: "TextHighlightBlue")!
+    public static let ForgetMode = UIColor(named: "ForgetMode")!
+    public static let ReaderModeSepia = UIColor(named: "ReaderModeSepia")!
+
+    // Functional Colors
+    public static let DarkGreen = UIColor(named: "DarkGreen")!
+    public static let BrightRed = UIColor(named: "BrightRed")!
+    public static let DarkRed = UIColor(named: "DarkRed")!
+    public static let NeutralGrey = UIColor(named: "NeutralGrey")!
+
+    public static let Grey10 = UIColor(named: "Grey10")!
+    public static let Grey20 = UIColor(named: "Grey20")!
+    public static let Grey30 = UIColor(named: "Grey30")!
+    public static let Grey40 = UIColor(named: "Grey40")!
+    public static let Grey50 = UIColor(named: "Grey50")!
+    public static let Grey60 = UIColor(named: "Grey60")!
+    public static let Grey70 = UIColor(named: "Grey70")!
+    public static let Grey80 = UIColor(named: "Grey80")!
+    public static let Grey90 = UIColor(named: "Grey90")!
+
+    public static let Blue40 = UIColor(named: "Blue40")!
+    public static let Blue50 = UIColor(named: "Blue50")!
+    public static let Blue60 = UIColor(named: "Blue60")!
+
+    //Contextual Onboarding Colors
+    public static let COLightBlue = UIColor(named: "COLightBlue")!
+    public static let CODarkBlue = UIColor(named: "CODarkBlue")!
+    public static let COLightRed = UIColor(named: "COLightRed")!
+    public static let CODarkRed = UIColor(named: "CODarkRed")!
+}
