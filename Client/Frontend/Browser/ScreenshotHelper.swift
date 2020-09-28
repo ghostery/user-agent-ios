ecosia/ios-browser

Missing screenshots (#6998)
* Fix issue where tabs in the main tab tray view show blank images

Client/Frontend/Browser/ScreenshotHelper.swift
@@ -4,6 +4,7 @@
 
 import Foundation
 import Shared
+import WebKit
 
 /**
  * Handles screenshots for a given tab, including pages with non-webview content.
@@ -16,22 +17,40 @@ class ScreenshotHelper {
     init(controller: BrowserViewController) {
         self.controller = controller
     }
-
+    
+    /// Takes a screenshot of the WebView to be displayed on the tab view page
+    /**
+     If taking a screenshot of the home page, uses our custom screenshot `UIView` extension function
+     If taking a screenshot of a website, uses apple's `takeSnapshot` function
+     */
     func takeScreenshot(_ tab: Tab) {
-        var screenshot: UIImage?
-
-        if let url = tab.url {
-            if InternalURL(url)?.isAboutHomeURL ?? false {
-                if let homePanel = controller?.firefoxHomeViewController {
-                    screenshot = homePanel.view.screenshot(quality: UIConstants.ActiveScreenshotQuality)
+        guard let webView = tab.webView, let url = tab.url else {
+            Sentry.shared.send(message: "Tab Snapshot Error", tag: .tabManager, severity: .debug, description: "Tab webView or url is nil")
+            return
+        }
+        //Handle home page snapshots, can not use Apple API snapshot function for this
+        if InternalURL(url)?.isAboutHomeURL ?? false {
+            if let homePanel = controller?.firefoxHomeViewController {
+                let screenshot = homePanel.view.screenshot(quality: UIConstants.ActiveScreenshotQuality)
+                tab.setScreenshot(screenshot)
+            }
+        //Handle webview screenshots
+        } else {
+            let configuration = WKSnapshotConfiguration()
+            //This is for a bug in certain iOS 13 versions, snapshots cannot be taken correctly without this boolean being set
+            if #available(iOS 13.0, *) {
+                configuration.afterScreenUpdates = false
+            }
+            webView.takeSnapshot(with: configuration) { image, error in
+                if let image = image {
+                    tab.setScreenshot(image)
+                } else if let error = error {
+                    Sentry.shared.send(message: "Tab snapshot error", tag: .tabManager, severity: .debug, description: error.localizedDescription)
+                } else {
+                    Sentry.shared.send(message: "Tab snapshot error", tag: .tabManager, severity: .debug, description: "No error description")
                 }
-            } else {
-                let offset = CGPoint(x: 0, y: -(tab.webView?.scrollView.contentInset.top ?? 0))
-                screenshot = tab.webView?.screenshot(offset: offset, quality: UIConstants.ActiveScreenshotQuality)
             }
         }
-
-        tab.setScreenshot(screenshot)
     }
 
     /// Takes a screenshot after a small delay.

ghostery/user-agent-ios

Client/Frontend/Browser/ScreenshotHelper.swift
@@ -4,6 +4,7 @@
 
 import Foundation
 import Shared
+import WebKit
 
 /**
  * Handles screenshots for a given tab, including pages with non-webview content.
@@ -16,22 +17,40 @@ class ScreenshotHelper {
     init(controller: BrowserViewController) {
         self.controller = controller
     }
-
+    
+    /// Takes a screenshot of the WebView to be displayed on the tab view page
+    /**
+     If taking a screenshot of the home page, uses our custom screenshot `UIView` extension function
+     If taking a screenshot of a website, uses apple's `takeSnapshot` function
+     */
     func takeScreenshot(_ tab: Tab) {
-        var screenshot: UIImage?
-
-        if let url = tab.url {
-            if InternalURL(url)?.isAboutHomeURL ?? false {
-                if let homePanel = controller?.homeViewController {
-                    screenshot = homePanel.view.screenshot(quality: UIConstants.ActiveScreenshotQuality)
+        guard let webView = tab.webView, let url = tab.url else {
+            Sentry.shared.send(message: "Tab Snapshot Error", tag: .tabManager, severity: .debug, description: "Tab webView or url is nil")
+            return
+        }
+        //Handle home page snapshots, can not use Apple API snapshot function for this
+        if InternalURL(url)?.isAboutHomeURL ?? false {
+            if let homePanel = controller?.homeViewController {
+                let screenshot = homePanel.view.screenshot(quality: UIConstants.ActiveScreenshotQuality)
+                tab.setScreenshot(screenshot)
+            }
+        //Handle webview screenshots
+        } else {
+            let configuration = WKSnapshotConfiguration()
+            //This is for a bug in certain iOS 13 versions, snapshots cannot be taken correctly without this boolean being set
+            if #available(iOS 13.0, *) {
+                configuration.afterScreenUpdates = false
+            }
+            webView.takeSnapshot(with: configuration) { image, error in
+                if let image = image {
+                    tab.setScreenshot(image)
+                } else if let error = error {
+                    Sentry.shared.send(message: "Tab snapshot error", tag: .tabManager, severity: .debug, description: error.localizedDescription)
+                } else {
+                    Sentry.shared.send(message: "Tab snapshot error", tag: .tabManager, severity: .debug, description: "No error description")
                 }
-            } else {
-                let offset = CGPoint(x: 0, y: -(tab.webView?.scrollView.contentInset.top ?? 0))
-                screenshot = tab.webView?.screenshot(offset: offset, quality: UIConstants.ActiveScreenshotQuality)
             }
         }
-
-        tab.setScreenshot(screenshot)
     }
 
     /// Takes a screenshot after a small delay.